c9d484f1ca6324a0d129c785ba93f3bee2512941
book-lover-man <eric.shvidky@gmail.com>
2025-04-25 17:39:26 +0000

    Intro to Intro

---
+++ dungeon.py
@@ -0,0 +1,5 @@
+print("Hi fellow soldier, you must be here to recover the Eel Crown"
+"Wait, you don't know?!? Your parents didn't tell you?!" 
+"Well, let me catch you up.")
+print("10 years ago there was a king called King Eel." \
+"He really valued PP (Pepperoni Pizza), one day he ordered peperoni pizza")
