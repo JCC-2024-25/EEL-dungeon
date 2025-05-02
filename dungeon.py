3815a3fdc3f2e47d1d103cb1a2e238df5077a6dc
book-lover-man <eric.shvidky@gmail.com>
2025-05-02 17:38:53 +0000

    rizz

--- dungeon.py
+++ dungeon.py
@@ -1,5 +1,12 @@
-print("Hi fellow soldier, you must be here to recover the Eel Crown"
-"Wait, you don't know?!? Your parents didn't tell you?!" 
-"Well, let me catch you up.")
-print("10 years ago there was a king called King Eel." \
-"He really valued PP (Pepperoni Pizza), one day he ordered peperoni pizza")
+name = input("Hi, I am McCheese. Here to inform you about your quest. What is your name?")
+input(f"Hi {name}, I see you have come to attempt the recovery of the Eel Crown (Enter)")
+input("Here is some info you need (Enter)")
+input("10 years ago there was a king called King Eel" 
+"He really valued PP (Pepperoni Pizza), so one day he ordered peperoni pizza "
+"and made sure there was pepproni on each slice (Enter)")
+input("But when the pizza was delivered there was 1 pepperoni in the middle (Enter)")
+input("So he got mad and used a powerful crown in his basement called the Eel Crown (Enter)")
+input("The Eel crown generates mobs of Eels that ar taught to destroy whatever " \
+"they see exept for him and they are attacking your city. (Enter)")
+input("So your job is to get the Eel crown and take down King Eel. (Enter)")
+input("So do you have what it takes? (Enter)")
