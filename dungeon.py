be16aa53beb2e8493c04cb3c2584db199d7bf295
book-lover-man <eric.shvidky@gmail.com>
2025-05-02 17:38:45 +0000

    rizz

--- dungeon.py
+++ dungeon.py
@@ -1,4 +1,15 @@
 import random
 path = ["1", "2", "3"]
 chosenpath = random.choice(path)
-print(You arrive )
+input("McCheese lets you past and you go through the door behind him.")
+input("You go down a long stone hallway engraved with beautiful carvings of eels doing various things")
+input("You keep going and you enter a large room with three doors")
+door = input("What door do you want to go through?(1,2,3)")
+if chosenpath = 2
+    input("There is an electric eel")
+
+class WaterEel:
+    def __init__(self, name, health, attackpower):
+            self.name = name
+            self.health = health
+            self.attack = attackpower
